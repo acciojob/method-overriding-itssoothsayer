eb5d803db18d4163632ca26e103a4e7260074df2
Afzal <af1626112@gmail.com>
2024-02-04 14:24:52 +0530

    1st attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,33 +1,27 @@
 package com.driver;
-// Task 1: Create class A with method `meth`
-// Task 1: Create class A with method `meth`
-
 
 public class Main {
 
     public static class A {
-        // Method in class A
-        String meth() {
+
+        public  String meth()
+        {
             return "Invoking method from class A";
         }
     }
 
-    // Task 2: Create class B which extends class A
     public static class B extends A {
-        // Task 4: Override method `meth` in class B
-        @Override
-        String meth() {
-            return "Method is overridden in Extended class B";
+        public  String meth()
+        {
+            return "Method is overridden in Extendend class B";
         }
     }
     public static void main(String[] args) {
-        // Task 3: Create an object of class B and call method `meth` of class A
-        B objB = new B();
-        String resultFromA = objB.meth();
+        B obj = new B();
+        obj.meth();
 
-
-        // Task 5: Call the overridden method `meth` from obj of class B
-        String resultFromB = objB.meth();
-
+        B obj1 = new B();
+        obj1.meth();
     }
+  
 }
